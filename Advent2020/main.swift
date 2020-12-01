ab05bbfd0e57255965d6e91253fea40876292d0d
Mark Lively <lively@accuweather.com>
2020-12-01 18:51:08 -0500

    Initial Commit

---
+++ Advent2020/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Advent2020
+//
+//  Created by Mark Lively on 12/1/20.
+//  Copyright © 2020 Livelys. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
